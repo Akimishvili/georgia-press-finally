ea64b29b6597979d23012ca61aa0afc57a91d546
Akimishvili <liliana.aqimishvili@gmail.com>
2024-04-13 07:31:25 +0400

    deleted nonexistent category select

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -58,9 +58,6 @@ class PageController extends Controller
         $youthCategory = $categories -> first(fn($category) => $category -> id == 8);
         $youth = $articles->where('visibility', '1')->filter(fn($article) => $article->categories->contains('id', $youthCategory -> id));
 
-        //TODO: culture category with articles
-        $cultureCategory = $categories -> first(fn($category) => $category -> id == 9);
-        $culture = $articles->where('visibility', '1')->filter(fn($article) => $article->categories->contains('id', $cultureCategory -> id));
 
         return view('pages.home',[
             'language' => App::getLocale(),
@@ -81,8 +78,6 @@ class PageController extends Controller
             'tourism' => $tourism,
             'youthCategory' => $youthCategory,
             'youth' => $youth,
-            'cultureCategory' => $cultureCategory,
-            'culture' => $culture,
             'blocks' => Block::whereNotNull('video')->with('article')->get()
         ]);
     }
