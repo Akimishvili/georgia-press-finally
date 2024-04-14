43ba3cfcc2e779b2b014f03f8ddc32928ef4ce3b
Akimishvili <liliana.aqimishvili@gmail.com>
2024-04-14 11:58:44 +0400

    fixed bug in category-item-component

--- resources/views/components/category-item-component.blade.php
+++ resources/views/components/category-item-component.blade.php
@@ -6,7 +6,7 @@
         <div class="col-sm-8">
             <div class="card-body">
                 <h5 class="card-title truncate" data-language="{{$language}}">{{ $item -> title -> $language }}</h5>
-                <p class="card-text line-clamp" data-language="{{$language}}">{!!  Str::substr( strip_tags($article -> description -> $language), 0, 150) !!}</p>
+                <p class="card-text line-clamp" data-language="{{$language}}">{!!  Str::substr( strip_tags($item -> description -> $language), 0, 150) !!}</p>
             </div>
         </div>
     </div>
